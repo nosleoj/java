c9ad20e986eee8c5db61eb1ac771ba494b6274d4
joel <joel@DESKTOP-O9VF6MA>
2025-08-20 23:05:40 -0300

    alteração

---
+++ primeiro.programa.java/src/curso/java/joel/Pessoa.java
@@ -0,0 +1,26 @@
+package curso.java.joel;
+
+public class Pessoa {
+
+	
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	private String sobrenome;
+	
+}
