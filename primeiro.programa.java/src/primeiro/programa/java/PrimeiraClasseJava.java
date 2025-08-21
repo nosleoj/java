69b1ff2fe3bb032864af897ba6bbad64ed7b61ed
joel <joel@DESKTOP-O9VF6MA>
2025-08-20 22:49:54 -0300

    f

--- primeiro.programa.java/src/primeiro/programa/java/PrimeiraClasseJava.java
+++ primeiro.programa.java/src/primeiro/programa/java/PrimeiraClasseJava.java
@@ -3,6 +3,6 @@ package primeiro.programa.java;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World new");
+		System.out.println("Hello World new boy");
 	}
 }
